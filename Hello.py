b4796d7aa3b91c0d92b8321476e45e3c8f368c45
reyzita <89064851+reyzita@users.noreply.github.com>
2024-03-05 16:24:10 +0700

    Streamlit_Project.py

--- Hello.py
+++ Hello.py
@@ -1,22 +1,79 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
 from streamlit.logger import get_logger
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+from io import StringIO
 
-LOGGER = get_logger(__name__)
+url = 'https://github.com/reyzita/dicoding-data-analysis-project/raw/96f046aa6003522df3ada08be5c7f6a1517b8ce9/pages/orders_df.csv'
 
+# Membaca file CSV dari URL dan memuatnya ke dalam DataFrame orders_df
+orders_df = pd.read_csv(url)
+
+
+### Streamlit Sidebar
+
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+
+
+### Streamlit Main
+
+st.header('Dicoding Data Analysis First Project Dashboard - Reyzita Afrida')
+st.subheader('E-Commerce Public Dataset')
+st.caption('Dataset: https://www.kaggle.com/datasets/olistbr/brazilian-ecommerce')
+
+
+##2. Bagaimana performa order "delivered" per tahun?
+st.subheader('Number of Purchases per Year with Status "Delivered"')
+
+orders_df['order_purchase_timestamp'] = pd.to_datetime(orders_df['order_purchase_timestamp'])
+# Filter data berdasarkan order_status 'delivered'
+delivered_orders = orders_df[orders_df['order_status'] == 'delivered']
+
+# Mengelompokkan data berdasarkan tahun dan menghitung jumlah pembelian
+purchase_per_year = delivered_orders.groupby(delivered_orders['order_purchase_timestamp'].dt.year)['order_id'].count()
+
+# Tampilkan grafik menggunakan Streamlit
+
+st.bar_chart(purchase_per_year)
+
+# Menampilkan nilai counts di atas setiap batang
+for i, value in enumerate(purchase_per_year):
+    st.text(f"Year: {purchase_per_year.index[i]}, Purchases: {value}")
+
+st.caption('Question: How is the performance of "delivered" orders per year?')
+st.caption('Conclusion: Based on the bar chart, it can be observed that the status of "Delivered" orders shows an increasing trend with the number in the year 2016 being 267, in the year 2017 being 43428, and in the year 2018 being 52783 "Delivered" orders.')
+
+##3. Bagaimana performa order "Delivered" pada tahun 2017?
+
+# Filter data for the year 2017 and status 'delivered'
+delivered_orders_2017 = orders_df[(orders_df['order_purchase_timestamp'].dt.year == 2017) & (orders_df['order_status'] == 'delivered')]
+
+# Group data by month and count the number of purchases
+purchase_per_month_2017 = delivered_orders_2017.groupby(delivered_orders_2017['order_purchase_timestamp'].dt.month)['order_id'].count()
+
+# Plot the number of purchases per month
+fig, ax = plt.subplots(figsize=(10, 6))
+purchase_per_month_2017.plot(kind='bar', color='darkgreen')
+
+st.subheader('Number of Purchases with Status "Delivered" per Month in 2017')
+
+# Add title and axis labels
+plt.xlabel('Month')
+plt.ylabel('Number of Purchases')
+
+# Display the counts above each bar
+for i, value in enumerate(purchase_per_month_2017):
+    plt.text(i, value + 5, str(value), ha='center', va='bottom')
+# Show the plot
+st.pyplot(fig)
+
+st.caption('Question: How is the performance of "Delivered" orders in 2017?')
+st.caption('Conclusion: Based on the bar chart, it can be observed that the number of "Delivered" order statuses does not exhibit a trend as it fluctuates throughout certain months. The highest number of orders with the status "Delivered" occurs in the 11th month, which is November. Meanwhile, the lowest number of orders with the status "Delivered" occurs in the 1st month, which is January.')
 
 def run():
     st.set_page_config(
@@ -24,28 +81,5 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 
-if __name__ == "__main__":
-    run()
